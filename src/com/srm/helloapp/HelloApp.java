cc030d5879c50302427343a581c7d5065bda1d12
Your Name <your.email@example.com>
2026-03-18 11:34:05 +0530

    UC4 - Display Hello with multiple command line arguments

--- src/com/srm/helloapp/HelloApp.java
+++ src/com/srm/helloapp/HelloApp.java
@@ -4,11 +4,15 @@ public class HelloApp {
 
     public static void main(String[] args) {
 
-        if(args.length > 0) {
-            System.out.println("Hello " + args[0]);
+        // If no arguments
+        if(args.length == 0) {
+            System.out.println("Hello World");
         } 
         else {
-            System.out.println("Hello World");
+            // Loop through all names
+            for(int i = 0; i < args.length; i++) {
+                System.out.println("Hello " + args[i]);
+            }
         }
 
     }
