c220250c22076161f936282bca854781311f2ca4
Your Name <your.email@example.com>
2026-03-18 11:21:38 +0530

    UC2 - Display Hello with command line argument

--- src/com/srm/helloapp/HelloApp.java
+++ src/com/srm/helloapp/HelloApp.java
@@ -1,9 +1,17 @@
 package com.srm.helloapp;
 
 public class HelloApp {
-	public static void main(String[] args) {
-		System.out.println("HELLO WORLD");
-		
-	}
+
+    public static void main(String[] args) {
+
+        // Check if name is provided
+        if(args.length > 0) {
+            System.out.println("Hello " + args[0]);
+        } 
+        else {
+            System.out.println("Hello");
+        }
+
+    }
 
 }
