ef3a80e89fe333292ca1ddf56cf3bbc9fe1b076e
Your Name <your.email@example.com>
2026-04-01 23:09:45 +0530

    UC7 - Display Hello using String.join

--- src/com/srm/helloapp/HelloAp.java
+++
@@ -1,8 +0,0 @@
-package com.srm.helloapp;
-
-public class HelloAp {
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
-	}
-
-}
---
+++ src/com/srm/helloapp/HelloApp.java
@@ -0,0 +1,15 @@
+package com.srm.helloapp;
+
+public class HelloApp {
+
+    public static void main(String[] args) {
+
+        if (args.length == 0) {
+            System.out.println("Hello World!");
+        } 
+        else {
+            String names = String.join(", ", args);
+            System.out.println("Hello " + names);
+        }
+    }
+}
